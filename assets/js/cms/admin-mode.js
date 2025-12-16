689267226a339c6d2e015b09a8db56672d29901a
kharchasaathi <abidalimohammad94@gmail.com>
2025-12-16 15:40:05 +0530

    Update admin-mode.js

--- assets/js/cms/admin-mode.js
+++ assets/js/cms/admin-mode.js
@@ -1,3 +1,17 @@
+import { auth } from "../core/firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const adminBar = document.getElementById("adminBar");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    document.body.classList.add("admin-mode");
+    adminBar.classList.remove("hidden");
+  } else {
+    document.body.classList.remove("admin-mode");
+    adminBar.classList.add("hidden");
+  }
+});
 const toolbar = document.getElementById("editorToolbar");
 
 onAuthStateChanged(auth, (user) => {
