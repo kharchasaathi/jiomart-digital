7acef695a0905a2f545931ee4e4ea0e1550f346c
kharchasaathi <abidalimohammad94@gmail.com>
2025-12-17 16:39:33 +0530

    Update live-sync.js

--- assets/js/site/live-sync.js
+++ assets/js/site/live-sync.js
@@ -3,7 +3,7 @@
  * Re-render page on state change
  ***************************************************/
 
-import { subscribe } from "../cms/state.js";
+
 import { renderPage } from "./render.js";
 
 subscribe(() => {
