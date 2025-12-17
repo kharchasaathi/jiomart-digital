a0c74a1eab747dc92066ec6626bc671915c08074
kharchasaathi <abidalimohammad94@gmail.com>
2025-12-17 10:18:14 +0530

    Update public.js

--- assets/js/site/public.js
+++ assets/js/site/public.js
@@ -1,15 +1,19 @@
 /***************************************************
- * PUBLIC ENTRY – NO ADMIN CODE
+ * PUBLIC ENTRY – GUARANTEED RENDER
  ***************************************************/
+import { loadPage } from "./cms/page-store.js";
+import { renderPage } from "./cms/render.js";
+import { setAdminMode } from "./cms/state.js";
 
-import { loadPage } from "../cms/page-store.js";
-import { renderPage } from "../cms/render.js";
-import { setAdminMode } from "../cms/state.js";
+console.log("🚀 Public entry loaded");
 
 // 🔒 Force public mode
 setAdminMode(false);
 
 (async function initPublic() {
+  console.log("📥 Loading page: home");
   await loadPage("home");
+
+  console.log("🎨 Rendering page");
   renderPage();
 })();
